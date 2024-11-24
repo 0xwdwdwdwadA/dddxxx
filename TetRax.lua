5dfabe68431308d715916bc069ad553cf6f60ceb
YEX <2214939320@qq.com>
2024-11-24 16:58:49 +0800

    Update TetRax.lua

--- TetRax.lua
+++ TetRax.lua
@@ -4,7 +4,7 @@ local Notify = AkaliNotif.Notify;
 Notify({
 Description = "检查中...";
 Title = "检查白名单";
-Duration = 5;
+Duration = 10;
 });
 local sound = Instance.new("Sound", workspace) do
 sound.SoundId = "rbxassetid://4590662766"
@@ -21,16 +21,6 @@ for i, v in pairs(HWIDTable) do
     if v == HWID then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/DINERO9/TXXK-9988332231C/refs/heads/main/TXX-0099998877ACSSSCRIv2"))()
     elseif v ~= HWID then
-Notify({
-Description = "请加群获得白名单:669173744";
-Title = "你没有白名单!";
-Duration = 10000;
-});
-local sound = Instance.new("Sound", workspace) do
-sound.SoundId = "rbxassetid://4590662766"
-sound.Volume = 3
-sound.PlayOnRemove = true
-sound:Destroy()
-end
+		print("你没有白名单!")
     end
 end
